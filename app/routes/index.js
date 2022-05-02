3896eefc3f3f61746d987ce827f711b1be4e6fa8
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2022-05-02 07:27:43 +0200

    added profile route

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,12 @@
 const sp_route = require('./spellcheck')
 const public_route = require('./public')
 const auth_route = require('./auth')
+const profile_route = require('./profile')
 
 function apiRoutes({ app, logger }) {
   app.use(sp_route({ logger }))
   app.use(auth_route({ logger }))
+  app.use(profile_route({ logger }))
 }
 
 function zakariRoutes({ app, logger }) {
