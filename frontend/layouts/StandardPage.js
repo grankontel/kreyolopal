75d129fffd1d97863f5fbbffcc68ea78acfe43ba
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2022-05-04 17:00:57 +0200

    moved layout

---
+++ frontend/layouts/StandardPage.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import { Container, Content } from 'react-bulma-components'
+import { Helmet } from 'react-helmet-async'
+import TopNavbar from '../components/TopNavbar'
+import findByType from '../lib/findByType'
+
+const _Footer = () => null
+const _Head = ({ title, description, children }) => null
+
+const StandardPage = ({ children, ...props }) => {
+  const PageFooter = () => {
+    const footer = findByType(children, _Footer)
+
+    return (
+      <Container
+        max
+        breakpoint="fullhd"
+        textAlign="center"
+        className="page-footer"
+        renderAs="footer"
+      >
+        {footer?.props.children}
+        <Content>&copy; TiMalo — 2022</Content>
+      </Container>
+    )
+  }
+
+  const PageHead = () => {
+    const _head = findByType(children, _Head, true)
+    return (_head !== undefined) ? (
+      <Helmet>
+        <title>{_head.props.title}</title>
+        <meta name="description" content={_head.props.description} />
+        {_head.props.children}
+      </Helmet>
+    ) : ' '
+    }
+
+  return (
+    <>
+    <PageHead />
+      <TopNavbar />
+
+      <Container {...props} className="main" renderAs="main">
+        {children}
+      </Container>
+
+      <PageFooter />
+    </>
+  )
+}
+
+StandardPage.Head = _Head
+StandardPage.Footer = _Footer
+
+export default StandardPage
