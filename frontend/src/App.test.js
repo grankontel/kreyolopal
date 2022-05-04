0e66326b2c2adcd56d3f5bc07c9a09c50bd15441
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2022-05-04 17:11:19 +0200

    clean up old frontend

--- frontend/src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Zakari/i);
-  expect(linkElement).toBeInTheDocument();
-});
