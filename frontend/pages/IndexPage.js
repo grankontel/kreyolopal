30278b6cd8eb2d113625a24e97ab311aa8f4c6ad
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2022-05-09 10:54:15 +0200

    better index

--- frontend/pages/IndexPage.js
+++ frontend/pages/IndexPage.js
@@ -1,8 +1,10 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 import {
+  Button,
   Columns,
   Container,
+  Content,
   Heading,
   Hero,
   Image,
@@ -10,8 +12,11 @@ import {
 } from 'react-bulma-components'
 import StandardPage from '../layouts/StandardPage'
 import ImageSet from '../components/ImageSet'
+import FeatureList from '../components/FeatureList'
 
 const IndexPage = () => {
+  const navigate = useNavigate()
+
   return (
     <StandardPage max breakpoint="fullhd">
       <StandardPage.Head
@@ -39,11 +44,54 @@ const IndexPage = () => {
           </Columns.Column>
         </Columns>
       </Hero>
-      <Container max breakpoint="fullhd">
+      <Container max breakpoint="desktop">
         <Section>
           <Heading renderAs="h2" size={3}>
             Le correcteur orthographique
           </Heading>
+          <Columns>
+            <Columns.Column size={4}>
+              <ImageSet
+                className="app_mockup mx-auto"
+                src="images/kreol_app2.png"
+                alt="Application mockup"
+              />
+            </Columns.Column>
+            <Columns.Column size={8}>
+              <Columns>
+                <Columns.Column size="three-quarters">
+                  <Content textSize={5} textFamily="secondary">
+                    <p>
+                      Le correcteur orthographique en ligne repere les erreurs
+                      dans la graphie du créole.
+                    </p>
+                    <p>
+                      Il est particulièrement utile pour ceux qui écrivent le
+                      créole de façon occasionnelle et veulent éviter les
+                      erreurs de graphie grossières.
+                    </p>
+                    <FeatureList>
+                      <FeatureList.Item icon="thumbs-up">
+                        Déjà près de 5000 mots de vocabulaire !
+                      </FeatureList.Item>
+                    </FeatureList>
+                  </Content>
+                  <Button.Group align="right">
+                    <Button
+                      mt={4}
+                      size="large"
+                      color="primary"
+                      onClick={() => {
+                        navigate('/spellcheck')
+                      }}
+                    >
+                      Essayer
+                    </Button>
+                  </Button.Group>
+                </Columns.Column>
+              </Columns>
+            </Columns.Column>
+          </Columns>
         </Section>
       </Container>
       <StandardPage.Footer>
