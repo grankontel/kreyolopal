065f4653d3ff79489ff006441718153aaa01a7f4
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2022-05-14 11:20:10 +0200

    chore:clean up

--- frontend/app.js
+++ frontend/app.js
@@ -29,7 +29,7 @@ export const App = () => {
             path="/account"
             element={
               <ZakProvider.Protected to="/login">
-                <AccountPage />{' '}
+                <AccountPage />
               </ZakProvider.Protected>
             }
           />
