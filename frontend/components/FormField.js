650750fcc01d47729499cb6dfffbd64e3aca7a3e
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2022-05-31 12:16:42 +0200

    prop types on FormField

--- frontend/components/FormField.js
+++ frontend/components/FormField.js
@@ -1,8 +1,9 @@
 import React from 'react'
 import { Form } from 'react-bulma-components'
+import PropTypes from 'prop-types'
 
 const FormField = (props) => {
-  const { label, value, name, type, autoComplete, handler } = props
+  const { label, value, name, type, autoComplete, setValue } = props
   return (
     <Form.Field>
       <Form.Label>{label}</Form.Label>
@@ -12,12 +13,29 @@ const FormField = (props) => {
           type={type}
           value={value}
           autoComplete={autoComplete || ''}
-          onChange={(e) => handler(e.target.value)}
+          onChange={(e) => setValue(e.target.value)}
           required
         />
       </Form.Control>
     </Form.Field>
   )
+}
+
+FormField.propTypes = {
+  label: PropTypes.string.isRequired,
+  value: PropTypes.node.isRequired,
+  name: PropTypes.string,
+  type: PropTypes.oneOf(['text', 'email', 'password']).isRequired,
+  autoComplete: PropTypes.string,
+  setValue: PropTypes.func,
+  required: PropTypes.bool,
+}
+
+FormField.defaultProps = {
+  type: 'text',
+  required: false,
+  autoComplete: 'on',
+  setValue: (e) => e.preventDefault(),
 }
 
 export default FormField
