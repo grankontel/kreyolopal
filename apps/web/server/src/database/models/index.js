2537c91cb814b3d8063cf25ca47afe032efa5789
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2022-06-15 00:02:47 +0200

    model static index

--- apps/web/server/src/database/models/index.js
+++ apps/web/server/src/database/models/index.js
@@ -1,12 +1,9 @@
 require('dotenv').config();
 
-const fs = require('fs');
-const path = require('path');
 const Sequelize = require('sequelize');
 
-const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/../config/config.js`)[env];
+const config = require('../config/config.js')[env];
 const db = {};
 
 let sequelize;
@@ -21,18 +18,13 @@ if (config.use_env_variable) {
   );
 }
 
-fs.readdirSync(__dirname)
-  .filter(
-    (file) =>
-      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
-  )
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(
-      sequelize,
-      Sequelize.DataTypes
-    );
-    db[model.name] = model;
-  });
+const _model00 = require('./rating')(sequelize, Sequelize.DataTypes);
+const _model01 = require('./spellchecked')(sequelize, Sequelize.DataTypes);
+const _model02 = require('./user')(sequelize, Sequelize.DataTypes);
+
+[_model00, _model01, _model02].forEach((model) => {
+  db[model.name] = model;
+});
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
