39e0f331416469c96aff9764ec0499fa805676ba
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2022-07-07 16:08:29 +0200

    add admin crud routes

--- apps/web/server/src/routes/index.js
+++ apps/web/server/src/routes/index.js
@@ -2,11 +2,13 @@ const sp_route = require('./spellcheck');
 const public_route = require('./public');
 const auth_route = require('./auth');
 const profile_route = require('./profile');
+const admin_route = require ('./admin');
 
 function apiRoutes({ app, logger }) {
   app.use(sp_route({ logger }));
   app.use(auth_route({ logger }));
   app.use(profile_route({ logger }));
+  app.use(admin_route({logger}));
 }
 
 function zakariRoutes({ app, logger }) {
