12f24ddd306fbc89ee263db097a5894211883774
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2022-09-25 13:19:04 +0200

    sort multiple responses

--- apps/web/server/src/routes/dico.js
+++ apps/web/server/src/routes/dico.js
@@ -36,15 +36,30 @@ const dico_route = ({ logger }) => {
   router.get('/suggest/:word', async (req, res) => {
     const word = req.params.word
 
+    const regex = new RegExp(`^${word}`, 'i')
+    
     const filter = {
-      variations: new RegExp(`^${word}`, 'i'),
+      variations: regex,
     }
     const projection = {
       entry: 1,
+      variations: 1,
     }
+
     const coll = client.db('dico_poullet').collection('words')
     const cursor = coll.find(filter, { projection })
-    const result = await cursor.toArray()
+    const unsorted = await cursor.toArray()
+    const result = unsorted.sort((a,b) => {
+        if (regex.test(a.entry) && regex.test(b.entry)) {
+          return a.length - b.length
+        }
+
+        if (regex.test(a.entry)) return -1
+        if (regex.test(b.entry)) return 1
+
+        return a.variations.findIndex(i => regex.test(i)) - b.variations.findIndex(i => regex.test(i))
+    })
+
     res.set('Content-Type', 'application/json')
     res.status(200).send(result)
   })
