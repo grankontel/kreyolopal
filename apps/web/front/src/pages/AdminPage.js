b6d3a6150115d3e111de8705165716ca1d693e6f
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2023-01-15 13:53:37 +0100

    updated front

--- apps/web/front/src/pages/AdminPage.js
+++ apps/web/front/src/pages/AdminPage.js
@@ -1,16 +1,21 @@
-import * as React from 'react'
-import { Admin, Resource, EditGuesser } from 'react-admin'
-import simpleRestProvider from 'ra-data-simple-rest'
-import { UserList, SpellcheckedList, RatingList } from '@kreyolopal/web-ui'
+import * as React from 'react';
+import { Admin, Resource, EditGuesser } from 'react-admin';
+import simpleRestProvider from 'ra-data-simple-rest';
+import { UserList, SpellcheckedList, RatingList } from '@kreyolopal/web-ui';
 
 const AdminPage = () => (
-  <Admin basename="/admin"
-    dataProvider={simpleRestProvider('/api/admin')}
-  >
-    <Resource name="users" list={UserList} edit={EditGuesser} />
-    <Resource name="spellcheckeds" list={SpellcheckedList} edit={EditGuesser} />
-    <Resource name="ratings" list={RatingList} edit={EditGuesser} />
-  </Admin>
-)
+    <Admin
+      basename="/admin"
+      dataProvider={simpleRestProvider('/api/admin')}
+    >
+      <Resource name="users" list={UserList} edit={EditGuesser} />
+      <Resource
+        name="spellcheckeds"
+        list={SpellcheckedList}
+        edit={EditGuesser}
+      />
+      <Resource name="ratings" list={RatingList} edit={EditGuesser} />
+    </Admin>
+);
 
-export default AdminPage
+export default AdminPage;
