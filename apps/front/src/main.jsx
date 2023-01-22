5c9c390eea5b3385ed141ec972d62197c85d648c
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2023-01-22 14:27:47 +0100

    vite main.jsx

--- apps/front/src/main.jsx
+++ apps/front/src/main.jsx
@@ -1,13 +1,17 @@
-import { StrictMode } from 'react';
-import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './app/App';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import {
+  BrowserRouter
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <StrictMode>
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </StrictMode>
-);
+
+  </React.StrictMode>
+)
