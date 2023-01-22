c96b9e3b19e24756c5d3fdeaee89816fea7973df
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2023-01-22 11:40:45 +0100

    remove need for wabap stuff

--- apps/web/server/src/routes/index.js
+++ apps/web/server/src/routes/index.js
@@ -4,14 +4,14 @@ const auth_route = require('./auth');
 const profile_route = require('./profile');
 const admin_route = require ('./admin');
 const dico_route = require('./dico')
-const wabap_route = require('./wabap')
+// const wabap_route = require('./wabap')
 
 function apiRoutes({ app, logger }) {
   app.use(sp_route({ logger }));
   app.use(auth_route({ logger }));
   app.use(profile_route({ logger }));
   app.use(dico_route({ logger }));
-  app.use(wabap_route({ logger }));
+//  app.use(wabap_route({ logger }));
   app.use(admin_route({logger}));
 }
 
