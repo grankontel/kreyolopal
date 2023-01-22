9e7bafe8bf64abe58eb9d6252acf4a2ac6a3b95a
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2023-01-22 15:08:06 +0100

    improved some services

--- apps/server/src/services/userService.js
+++ apps/server/src/services/userService.js
@@ -1,85 +1,88 @@
+import logger from './logger.js'
+import authService from './authService.js'
+import config from '../config.js'
 const Promise = require('bluebird')
-const logger = require('./logger')
-const authService = require('./authService')
-const db = require('../database/models')
+const argon2 = require('argon2')
+const jwt = require('jsonwebtoken')
+const { SHA3 } = require('sha3')
+import db from '../database/models'
 
 const { sequelize } = db
 const user = db.User
 
-const userService = {
-  /**
-   * Find a user by his email
-   * @param {string} email Email of the user to find
-   */
-  findbyEmail(email) {
-    return user
-      .findOne({ where: { email } })
-      .then(
-        (record) => record,
-        (reason) => {
-          logger.error(reason)
-          return null
-        }
-      )
-      .catch((error) => {
-        logger.error(error)
+const findbyEmail = (email) => {
+  return user
+    .findOne({ where: { email } })
+    .then(
+      (record) => record,
+      (reason) => {
+        logger.error(reason)
         return null
-      })
-  },
-
-  resetPwdToken(email) {
-    const me = this
-    return new Promise((resolve, reject) => {
-      me.findbyEmail(email).then((record) => {
-        if (!record) reject(new Error(`cannot find user for : ${email}`))
-
-        record.reset_pwd_token = authService.generateVerifToken(record.email)
-        record.save()
-        resolve(record)
-      })
+      }
+    )
+    .catch((error) => {
+      logger.error(error)
+      return null
     })
-  },
-
-  register(record) {
-    return new Promise((resolve, reject) => {
-      sequelize.sync().then(
-        () => {
-          const myrecord = record
-          myrecord.email_verif_token = authService.generateVerifToken(
-            record.email
-          )
-
-          authService
-            .hashPassword(record.password)
-            .then(
-              (hashedpwd) => {
-                myrecord.password = hashedpwd
-                return myrecord
-              },
-              (error) => reject(error)
-            )
-            .then((_record) => {
-              user
-                .create(_record)
-                .then(
-                  (aUser) => {
-                    resolve(aUser)
-                  },
-                  (reason) => {
-                    logger.error('register rejected')
-                    reject(reason.errors[0].message)
-                  }
-                )
-                .catch((error) => {
-                  logger.error('register exception')
-                  reject(error)
-                })
-            })
-        },
-        (reason) => reject(reason)
-      )
-    })
-  },
 }
 
-module.exports = userService
+const resetPwdToken = (email) => {
+  return new Promise((resolve, reject) => {
+    findbyEmail(email).then((record) => {
+      if (!record) reject(new Error(`cannot find user for : ${email}`))
+
+      record.reset_pwd_token = authService.generateVerifToken(record.email)
+      record.save()
+      resolve(record)
+    })
+  })
+}
+
+const register = (record) => {
+  return new Promise((resolve, reject) => {
+    sequelize.sync().then(
+      () => {
+        const myrecord = record
+        myrecord.email_verif_token = authService.generateVerifToken(
+          record.email
+        )
+
+        authService
+          .hashPassword(record.password)
+          .then(
+            (hashedpwd) => {
+              myrecord.password = hashedpwd
+              return myrecord
+            },
+            (error) => reject(error)
+          )
+          .then((_record) => {
+            user
+              .create(_record)
+              .then(
+                (aUser) => {
+                  resolve(aUser)
+                },
+                (reason) => {
+                  logger.error('register rejected')
+                  reject(reason.errors[0].message)
+                }
+              )
+              .catch((error) => {
+                logger.error('register exception')
+                reject(error)
+              })
+          })
+      },
+      (reason) => reject(reason)
+    )
+  })
+}
+
+export const userService = {
+  findbyEmail,
+  resetPwdToken,
+  register,
+}
+
+export default userService
