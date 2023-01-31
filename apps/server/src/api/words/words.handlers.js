c2e7b99cfa20682706d28714d0964924e913318b
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2023-01-31 17:59:54 +0100

    handle query parameters

--- apps/server/src/api/words/words.handlers.js
+++ apps/server/src/api/words/words.handlers.js
@@ -18,7 +18,53 @@ const sanitizeEntry = (src) => {
 }
 
 const getWord = async function (req, res) {
-  return wordModel.find().then((results) => res.send(results))
+  var filterObj = {}
+  if (req.query?.filter) {
+    try {
+      logger.info(`filter = ${req.query.filter}`)
+      filterObj = JSON.parse(req.query.filter)
+      logger.info(`filtering on ${JSON.stringify(filterObj)}`)
+    } catch (e) {
+      logger.error(`Error on parsing filter query elements : ${e}`)
+    }
+  }
+
+  var findPromise = wordModel.find(filterObj)
+  if (req.query?.range) {
+    try {
+      logger.info(`range  = ${req.query.range}`)
+      const [offset, limit] = JSON.parse(req.query.range)
+      findPromise = offset > 0 ? findPromise.skip(offset) : findPromise
+      findPromise = findPromise.limit(limit)
+      logger.info(`range is from ${offset} to ${limit}`)
+    } catch (e) {
+      logger.error(`Error on parsing range query elements : ${e}`)
+      findPromise = findPromise.limit(10)
+    }
+  }
+  if (req.query?.sort) {
+    try {
+      logger.info(`sort  = ${req.query.sort}`)
+      const [field, order] = JSON.parse(req.query.sort)
+      var sortObj = {}
+      sortObj[field] = order == 'ASC' ? 1 : -1
+      findPromise = findPromise.sort(sortObj)
+      logger.info(`sorting by ${JSON.stringify(sortObj)}`)
+    } catch (e) {
+      logger.error(`Error on parsing sort query elements : ${e}`)
+    }
+  }
+  return findPromise
+    .then(
+      (results) => res.send(results),
+      (reason) => {
+        logger.error(reason)
+        return res.status(500).send({ status: 'error', error: [reason] })
+      }
+    )
+    .catch((_error) => {
+      res.status(500).send({ status: 'error', error: [_error] })
+    })
 }
 
 const postWord = async function (req, res) {
@@ -30,7 +76,10 @@ const postWord = async function (req, res) {
     if (err) {
       logger.error(err)
       if (err.code === 11000) {
-        return res.status(409).json({ status: 'error', error: `Entry '${src.entry}' already exists` })
+        return res.status(409).json({
+          status: 'error',
+          error: `Entry '${src.entry}' already exists`,
+        })
       }
       return res.status(500).json({ status: 'error', error: 'Internal error' })
     }
