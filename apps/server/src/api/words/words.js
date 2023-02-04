8ca792858b21dd1bd888ae8a8f01ba5771e0a4df
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2023-02-04 20:19:04 +0100

    can use extra fields

--- apps/server/src/api/words/words.js
+++ apps/server/src/api/words/words.js
@@ -1,87 +1,93 @@
 import mongoose from 'mongoose'
 
-const definitionSchema = mongoose.Schema({
-  nature: {
-    type: [
-      {
+const definitionSchema = mongoose.Schema(
+  {
+    nature: {
+      type: [
+        {
+          type: 'String',
+          enum: [
+            'adjectif',
+            'adverbe',
+            'article',
+            'conjonction',
+            'exclamation',
+            'expression',
+            'interjection',
+            'locution',
+            'nom',
+            'nom propre',
+            'nombre',
+            'particule',
+            'préfixe',
+            'préposition',
+            'pronom',
+            'suffixe',
+            'verbe',
+          ],
+          required: true,
+        },
+      ],
+    },
+    meaning: {
+      gp: {
         type: 'String',
-        enum: [
-          'adjectif',
-          'adverbe',
-          'article',
-          'conjonction',
-          'exclamation',
-          'expression',
-          'interjection',
-          'locution',
-          'nom',
-          'nom propre',
-          'nombre',
-          'particule',
-          'préfixe',
-          'préposition',
-          'pronom',
-          'suffixe',
-          'verbe',
-        ],
-        required: true,
       },
-    ],
+      fr: {
+        type: 'String',
+      },
+    },
+    usage: {
+      type: ['String'],
+    },
+    synonyms: {
+      type: ['String'],
+    },
+    quotes: {
+      type: ['String'],
+    },
   },
-  meaning: {
-    gp: {
+  { timestamps: true }
+)
+
+const wordSchema = mongoose.Schema(
+  {
+    entry: {
       type: 'String',
+      required: true,
+      unique: true,
     },
-    fr: {
-      type: 'String',
+    variations: {
+      type: ['String'],
+      index: true,
     },
-  },
-  usage: {
-    type: ['String'],
-  },
-  synonyms: {
-    type: ['String'],
-  },
-  quotes: {
-    type: ['String'],
-  },
-}, { timestamps: true })
-
-const wordSchema = mongoose.Schema({
-  entry: {
-    type: 'String',
-    required: true,
-    unique: true,
-  },
-  variations: {
-    type: ['String'],
-    index: true
-  },
-  definitions: {
-    gp: {
-      type: [definitionSchema],
+    definitions: {
+      gp: {
+        type: [definitionSchema],
+      },
+    },
+    publishedAt: {
+      type: 'Date',
+      default: null,
+      index: true,
     },
   },
-  publishedAt: {
-    type: 'Date',
-    default: null,
-    index: true
-  }
-}, { timestamps: true })
+  { strict: false, timestamps: true }
+)
 
-wordSchema.method('toClient', function() {
-    var obj = this.toObject();
-    obj.definitions.gp.forEach((def)=> {
-      //Rename fields
-      def.id = def._id;
-      delete def._id;
-    })
-
+wordSchema.method('toClient', function () {
+  var obj = this.toObject()
+  obj.definitions.gp.forEach((def) => {
     //Rename fields
-    obj.id = obj._id;
-    delete obj._id;
+    def.id = def._id
+    delete def._id
+  })
 
-    return obj;
-});
+  //Rename fields
+  obj.id = obj._id
+  delete obj._id
+
+  return obj
+})
 
 export const wordModel = mongoose.model('Word', wordSchema)
