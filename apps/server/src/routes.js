cd0e68c48c1ae9776dfb2565b630db65ea114657
Thierry MALO <thierry.malo@(anti-spam)gmail.com>
2023-02-06 19:11:45 +0100

    moved adminMiddleware

--- apps/server/src/routes.js
+++ apps/server/src/routes.js
@@ -4,21 +4,9 @@ import profileRoutes from './api/profile'
 import authRoutes from './api/auth'
 import spellRoutes from './api/spellcheck'
 import contactRoutes from './api/contact'
-import logger from './services/logger'
-const cors = require('cors')
+import adminMiddleware from './middlewares/admin.middleware'
 
-function adminMiddleware(req, res, next) {
-  if (!req.user?.is_admin) {
-    logger.error(`${req.user?.firstname} ${req.user?.lastname} is not admin`)
-    return res.status(401).json({
-      status: 'error',
-      code: 401,
-      message: 'Unauthorized',
-      error: new Error('Unauthorized'),
-    })
-  }
-  next()
-}
+const cors = require('cors')
 
 function setRoutes({ app }) {
   // wire up to the express app
